1f69cdc31999fb2b749a4d62c323a40709f067fa
Kevin Nakamura <grinkers@grinkers.net>
2026-03-27 23:11:56 +0900

    Introduce InternalParser trait for Node vs EDN

--- tests/read.rs
+++ tests/read.rs
@@ -263,3 +263,10 @@ fn tagged() {
     )
   );
 }
+
+#[test]
+fn discard_tagged_values() {
+  assert_eq!(edn::read_string("[#_ #foo 1]").unwrap(), Edn::Vector(vec![]));
+  assert_eq!(edn::read_string("[#_ #foo 1 2]").unwrap(), Edn::Vector(vec![Edn::Int(2)]));
+  assert_eq!(edn::read_string("#_ #foo 1").unwrap(), Edn::Nil);
+}
